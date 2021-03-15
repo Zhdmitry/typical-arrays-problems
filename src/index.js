efdb1b09501cc1ca74691f3286908b2fd7b6b65a
Zhdmitry <zhoultko@gmail.com>
2021-03-15 19:57:47 +0300

    feat: edit js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,65 @@
 
 exports.min = function min (array) {
-  return 0;
+
+  if(Array.isArray(array) == false){
+    return 0;
+  }
+  else{ 
+    let arr = Array.from(array);
+        if (arr.length == 0 ) {
+      return 0;
+    }  
+    else{
+      let min = array[0];
+    for (let i = 0; i < array.length; i++) {
+        if (min > array[i]) min = array[i];
+    }
+    return min;
+     
+    }
+
+  }
+ 
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(Array.isArray(array) == false){
+    return 0;
+  }
+  else{ 
+    let arr = Array.from(array);
+        if (arr.length == 0 ) {
+      return 0;
+    }  
+    else{
+      let max = array[0];
+    for (let i = 0; i < array.length; i++) {
+        if (max < array[i]) max = array[i];
+    }
+    return max;
+     
+    }
+
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(Array.isArray(array) == false){
+    return 0;
+  }
+  else{ 
+    let arr = Array.from(array);
+        if (arr.length == 0 ) {
+      return 0;
+    }  
+    else{
+      let x = 0;
+    for (let i = 0; i < array.length; i++) {
+        x=x+arr[i];
+    }
+    return x/arr.length;
+     
+    }
+
+  }
 }
